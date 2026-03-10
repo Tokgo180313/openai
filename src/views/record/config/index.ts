f6d242e3270e40d8733d215b910d2d17aebaf9f2
Tokgo180313 <zyh180313@163.com>
2026-03-10 17:43:50 +0800

    2026-03-10: record 0.2

---
+++ src/views/record/config/index.ts
@@ -0,0 +1,38 @@
+const columns = [
+  {
+    title: "用户ID",
+    dataIndex: "userId",
+    key: "userId",
+  },
+  {
+    title: "用户昵称",
+    dataIndex: "userName",
+    key: "userName",
+  },
+  {
+    title: "用户账号",
+    dataIndex: "account",
+    key: "account",
+  },
+  {
+    title: "模型名称",
+    dataIndex: "modelName",
+    key: "modelName",
+  },
+  {
+    title: "分类",
+    dataIndex: "classify",
+    key: "classify",
+  },
+  {
+    title: "操作类型",
+    dataIndex: "recordType",
+    key: "recordType",
+  },
+  {
+    title: "操作时间",
+    dataIndex: "createTime",
+    key: "createTime",
+  },
+];
+export  default{ columns };
--- src/views/record/index.vue
+++ src/views/record/index.vue
@@ -1,10 +1,60 @@
 <template>
- 
+  <div class="record-container">
+    <div class="content">
+      <a-table
+        :data-source="recordList"
+        :columns="columnsList"
+        bordered
+        striped
+        size="small"
+      ></a-table>
+    </div>
+    <div class="pagination">
+      <a-pagination
+        :pageSize="pagination.pageSize"
+        :current="pagination.current"
+        :total="pagination.total"
+      ></a-pagination>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 // 操作日志
+import { computed, onMounted, reactive, ref } from "vue";
+import api from "@/api/apiList";
+let { findRecordListInterface } = api;
+import config from "./config";
+const { columns } = config;
+interface recordType {
+  id: string;
+  userId: string;
+  userName: string;
+  account: string;
+  modelName: string;
+  classify: string;
+  recordType: string;
+  createTime: Date;
+}
+const pagination = reactive({
+  current: 1,
+  pageSize: 10,
+  total: 0,
+});
+onMounted(() => {
+  getRecordList();
+});
+const columnsList = ref(columns);
+const recordList = ref<recordType[]>([]);
+const getRecordList = () => {
+  findRecordListInterface().then((res) => {
+    recordList.value = res.data;
+  });
+};
 </script>
 
 <style scoped lang="scss">
+.pagination {
+  text-align: right;
+}
 </style>
