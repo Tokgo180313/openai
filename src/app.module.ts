f5d384cf29fde9d72a929aedf8a907eade15c8a6
Tokgo180313 <zyh180313@163.com>
2026-05-29 11:43:45 +0800

    2026-05-29: mongodb 数据库名 更改

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,9 +49,9 @@ import { MessageAttachmentModule } from './message-attachments/message-attachmen
         logging: config.get<string>('NODE_ENV') === 'development',
       }),
     }),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest', {
-      autoIndex: true,
-    }),
+    MongooseModule.forRoot(config.get<string>('MONGO_URI'), {
+      autoIndex: true, // 自动索引
+    });
     ChatModule,
     UserModule,
     FileModule,
